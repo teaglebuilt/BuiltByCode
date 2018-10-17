8c8f85e9037d4b528a9217dc0f7b41f33359d6a7
teaglebuilt <dillan.teagle.va@gmail.com>
2018-10-17 15:11:21 -0500

    login/register validation

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # BuiltByCode
 
 Social blog platform for developers. Built with the MERN stack.
+(Mongoose, Express, React/Redux, Node.JS)
+
+# Dependencies
+
+    bcryptjs: 2.4.3,
+    body-parser: 1.18.3,
+    express: 4.16.4,
+    gravatar: 1.6.0,
+    jsonwebtoken: 8.3.0,
+    mongoose: 5.3.4,
+    passport: 0.4.0,
+    passport-jwt: 4.0.0,
+    validator: 10.8.0
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
@@ -11,21 +14,35 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-// connect to MongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
-  .catch(error => console.log(error));
+  .catch(err => console.log(err));
 
-app.get("/", (request, response) => response.send("Hello!"));
+// Passport middleware
+app.use(passport.initialize());
 
-//Use Routes
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+// app.use(express.static('client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const port = process.env.PORT || 5000;
 
