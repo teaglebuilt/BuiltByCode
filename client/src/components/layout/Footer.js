f66d343d596c1bc6b81e669435ca70d9daa3c852
teaglebuilt <dillan.teagle.va@gmail.com>
2018-10-23 11:19:02 -0500

    client side user auth

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer class="bg-dark text-white mt-5 p-4 text-center">
+      <footer className="bg-dark text-white mt-5 p-4 text-center">
         Copyright &copy; {new Date().getFullYear()} | Dillan Teagle
       </footer>
     </div>
