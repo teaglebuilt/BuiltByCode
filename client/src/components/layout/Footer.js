7c1aae29029d1d7f8f33516819ed4aca66b52330
teaglebuilt <dillan.teagle.va@gmail.com>
2018-10-27 12:44:03 -0500

    finished functionality

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,43 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <div>
-      <footer className="bg-dark text-white mt-5 p-4 text-center">
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+class Footer extends Component {
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+    console.log(this.props.auth);
+    const authFooter = (
+      <footer className="footer mt-5 bg-dark text-white p-4 text-center">
         Copyright &copy; {new Date().getFullYear()} | Dillan Teagle
       </footer>
-    </div>
-  );
+    );
+    const guestFooter = (
+      <footer className="footer bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; {new Date().getFullYear()} | Dillan Teagle
+      </footer>
+    );
+
+    return (
+      <div>
+        <div>
+          {/* <footer className="footer mt-5 p-4 text-center">
+            Copyright &copy; {new Date().getFullYear()} | Dillan Teagle
+          </footer> */}
+          {isAuthenticated ? authFooter : guestFooter}
+        </div>
+      </div>
+    );
+  }
 }
+
+Footer.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Footer);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,37 +1,44 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { clearCurrentProfile } from "../../actions/profileActions";
-import PropTypes from "prop-types";
 
 class Navbar extends Component {
-  onLogout(event) {
-    event.preventDefault();
-    // call logout action
+  onLogoutClick(e) {
+    e.preventDefault();
     this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
+
   render() {
     const { isAuthenticated, user } = this.props.auth;
+
     const authLinks = (
       <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/feed">
+            Post Feed
+          </Link>
+        </li>
         <li className="nav-item">
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
         </li>
         <li className="nav-item">
-          <a href="#" onClick={this.onLogout.bind(this)} className="nav-link">
+          <a
+            href=""
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-link"
+          >
             <img
               className="rounded-circle"
               src={user.avatar}
               alt={user.name}
-              style={{
-                width: "50px",
-                marginRight: "5px"
-              }}
-              title="You must have a gravatar connected to your email to display an image"
+              style={{ width: "25px", marginRight: "5px" }}
+              title="You must have a Gravatar connected to your email to display an image"
             />{" "}
             Logout
           </a>
@@ -53,36 +60,35 @@ class Navbar extends Component {
         </li>
       </ul>
     );
-    return (
-      <div>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-          <div className="container">
-            <Link className="navbar-brand" to="/">
-              Built By Code
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#mobile-nav"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
 
-            <div className="collapse navbar-collapse" id="mobile-nav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profiles">
-                    {" "}
-                    Developers
-                  </Link>
-                </li>
-              </ul>
-              {isAuthenticated ? authLinks : guestLinks}
-            </div>
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            DevConnector
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles">
+                  {" "}
+                  Developers
+                </Link>
+              </li>
+            </ul>
+            {isAuthenticated ? authLinks : guestLinks}
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
@@ -92,16 +98,11 @@ Navbar.propTypes = {
   auth: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => {
-  return {
-    auth: state.auth
-  };
-};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
 export default connect(
   mapStateToProps,
-  {
-    logoutUser,
-    clearCurrentProfile
-  }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
